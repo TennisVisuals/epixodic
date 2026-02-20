c081b5f174f210720859eb890b973e1765ed9f70
CourtHive <courthive@gmail.com>
2026-02-20 13:21:29 -0500

    style: :lipstick: update css for stroke sliders in landscape view

--- src/events/strokeSlider.ts
+++ src/events/strokeSlider.ts
@@ -9,14 +9,15 @@ export function strokeSlider(show?: string) {
   };
 
   if (show) {
+    const sliderWidth = Math.min(width * 0.5, 200);
     if (stroke_slider) {
       stroke_slider.style.display = 'flex';
-      stroke_slider.style.left = show == 'left' ? width * -1 + 'px' : width * 1.5 + 'px';
+      stroke_slider.style.left = show == 'left' ? -sliderWidth + 'px' : width + 'px';
     }
     if (slideLeft) slideLeft.style.display = show == 'right' ? 'flex' : 'none';
     if (slideRight) slideRight.style.display = show == 'left' ? 'flex' : 'none';
 
-    stroke_slider.style.left = show === 'left' ? '0px' : width * 0.5 + 'px';
+    stroke_slider.style.left = show === 'left' ? '0px' : (width - sliderWidth) + 'px';
     stroke_slider.style.animation = show === 'left' ? 'slideInLeft 0.7s' : 'slideInRight 0.7s';
   } else {
     setTimeout(() => hideSlide(), 100);
